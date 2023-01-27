4064a050267a73ac9597e4e770bd6ae00b9549fa
raghavendra singh <103146867+raghavendra7992@users.noreply.github.com>
2023-01-27 12:38:07 +0530

    update

--- src/routes/game.route.js
+++ src/routes/game.route.js
@@ -1,25 +1,19 @@
-const express = require("express");
-const Game = require("../model/game.model");
-const randomWords = require("random-words");
-
+const express = require('express');
 const app = express.Router();
 
-app.get("/", (req, res) => {
-  try {
-    let word = randomWords();
-    res.send(word);
-  } catch (e) {
-    res.status(500).send(e.message);
-  }
-});
 
-app.post("/", async (req, res) => {
-  try {
-    let score = await Game.create({ ...req.body });
-    res.send(score);
-  } catch (e) {
-    res.status(500).send(e.message);
-  }
-});
+app.get("/",(req,res)=>{
+    let arr =  ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," "]
+    let num = Math.floor(Math.random()*10)
+    if(num===1){
+        num = Math.floor(Math.random()*10)
+    }
+    let word = ""
+    for(let i=0;i<num;i++){
+        let index = Math.floor(Math.random()*27)
+        word += arr[index]
+    }
+    res.send(word)
+})
 
-module.exports = app;
+module.exports = app
